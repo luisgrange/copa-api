615ae4b6cc6329a557272e7289a935c00c72bc9f
André Elisei <andre.elisei@outlook.com>
2022-09-29 20:59:50 -0300

    Revert "Teste Banco"
    This reverts commit f75b68b10d9a672f7e54587bd588a443d6b6d078.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,31 +1,3 @@
-import { Router, urlencoded } from 'express';
-import { grupoController } from './controller/grupoController';
+import { Router } from 'express';
 
 export const router = Router();
-
-const data = [
-    {
-        "Grupo A":[
-            {
-                "Nome":"Brasil",
-                "Escudo":"Imagem do brasil",
-                "Pts":0,
-                "GM":0,
-                "GC":0,
-                "SG":0,
-                "PJ":0,
-                "Vit":0,
-                "Der":0,
-                "Emp":0
-
-            }
-        ]
-    }
-]
-
-const grupo = new grupoController();
-
-router.post('/api/v1/cadastro', grupo.handle);
-
-
-router.get("/api/v1/grupos", grupo.handleSelecao)
--- server/src/server.ts
+++ server/src/server.ts
@@ -4,6 +4,5 @@ import { router } from './routes';
 const app = express();
 const PORT = 3001;
 
-app.use(express.json());
-app.use(router);
-app.listen(PORT, () => {console.log(`Server running at http://localhost:${PORT}`)});
+
+app.listen(PORT, () => {console.log(`Server running at http://localhost:${PORT}`)})
