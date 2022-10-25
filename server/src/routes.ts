70bb301baa010449aaacc22b3f36fba583446403
luisgrange <luisfernando237grange@outlook.com>
2022-10-25 20:36:42 -0300

    Add controller

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
+import { SelecaoController } from './controller/selecaoController';
 
 export const router = Router();
 
 
+
+
+const createSelecao = new SelecaoController();
+
+// ROTA PARA CADASTRO DE SELEÇÕES
+router.post('/api/v1/selecao-CADASTRO', createSelecao.handleSelecoes);
+
+// ROTA PARA VISUALIZAÇÃO DAS SELEÇÕES
 router.get('/api/v1/selecoes', (req, res) => {
-    return res.status(200).json({"message": "hellos"});
+    return res.status(200).json({"message": "hello"});
 })
