55a84b6a6a5e31b407c83f115c898c3cf21ea489
André Elisei <andre.elisei@outlook.com>
2022-10-26 19:59:04 -0300

    Novas Rotas
    Novas Rotas

---
+++ server/src/controller/selecaoController.ts
@@ -0,0 +1,59 @@
+import {Request, Response} from 'express';
+import {client} from '../database/primaClient';
+
+
+export class SelecaoController{
+    public async handleSelecoes (req: Request, res: Response){
+        const  {
+            id,
+            escudo,
+            nome,
+            pts,  
+            vit,
+            emp,   
+            der,
+            gp,
+            gc,
+            sg     
+        } = req.body;
+
+        const selecao = await client.selecoes.create({
+            data: {
+                id,
+                escudo,
+                nome,
+                pts,  
+                vit,
+                emp,   
+                der,
+                gp,
+                gc,
+                sg  
+            }
+        });
+
+        return res.json(selecao);
+    }
+
+    public async handleGetSelecoes (req: Request, res: Response){
+        const selecoes = await client.selecoes.findMany();
+
+        
+        return res.status(200).json({selecoes});
+    }
+
+    public async handleGetSelecao (req: Request, res: Response){
+        const id = req.params.id;
+        const selecao = await client.selecoes.findUnique(
+            {
+                where:{
+                    id:Number(id) 
+                }
+            }
+        );
+
+        return res.json(selecao);
+    }
+      
+
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from 'express';
+import { SelecaoController } from './controller/selecaoController';
 
 export const router = Router();
 
 
-router.get('/api/v1/selecoes', (req, res) => {
-    return res.status(200).json({"message": "hellos"});
-})
+const createSelecao = new SelecaoController();
+
+// ROTA PARA CADASTRO DE SELEÇÕES
+router.post('/api/v1/selecao-CADASTRO', createSelecao.handleSelecoes);
+
+// ROTA PARA VISUALIZAÇÃO DAS SELEÇÕES
+router.get('/api/v1/selecoes',createSelecao.handleGetSelecoes);
+
+// ROTA PARA VISUALIZAÇÃO DA SELEÇÃO ÚNICA
+router.get('/api/v1/selecao/id/:id',createSelecao.handleGetSelecao);
