eee38de17ab35de88864a542c7007018399da156
luisgrange <luisfernando237grange@outlook.com>
2022-11-16 19:18:20 -0300

    Removendo importações

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,11 @@
 import express from "express";
 import { router } from './routes/routes';
-import swaggerUi from 'swagger-ui-express';
 
-import doc from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(doc))
 
 export {app}
